d0d8a73f66cf32ab5c3a2a3685874d2e1fd7d1a3
Alex Dunker <adunker@purdue.edu>
2016-01-14 17:34:01 -0500

    Initial commit.

---
+++ HW1/cipher.py
@@ -0,0 +1,15 @@
+"""
+Author:        Alex Dunker
+Email:         adunker@purdue.edu
+Title:         HW1
+Filename:      cipher.py
+Description:   Reads an input from a file called 'input.txt' and writes to
+               a file called 'output.txt'. Reads an encryption file called key
+               'key.txt'. 
+Date Created:  01/15/2016
+"""
+
+#!/usr/bin/env python
+
+
+
