d8836afdba2a70329710f5362e31215f3485f564
Alex Dunker <adunker@purdue.edu>
2016-01-15 20:28:53 -0500

    HW1

--- HW1/cipher.py
+++ HW1/cipher.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+
 """
 Author:        Alex Dunker
-Email:         adunker@purdue.edu
+ECN:           adunker
 Title:         HW1
 Filename:      cipher.py
 Description:   Reads an input from a file called 'input.txt' and writes to
@@ -9,7 +11,29 @@ Description:   Reads an input from a file called 'input.txt' and writes to
 Date Created:  01/15/2016
 """
 
-#!/usr/bin/env python
+
+def encrypt(key, message):
+    out = ""
+    x = 0
+    for symbol in message:
+        out += chr(((ord(symbol)+ord(key[x])) % 26) + ord('A'))
+        x += 1
+        if x == len(key):
+            x = 0
+    return out
+
+def letters(input):
+    return ''.join(filter(str.isalpha, input))
+
+def main():
+    f = open('input.txt', 'r')
+    inp = letters(f.read().upper())
+    f = open('key.txt', 'r')
+    key = f.read().strip().upper()
+    out = encrypt(key, inp)
+    f = open('output.txt', 'w')
+    f.write(out)
+    f.close()
 
 
-
+main()
---
+++ HW1/cryptBreak.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+### Author: Alex Dunker
+### ECN: adunker
+### HW:
+### Filename:	
+### Due Date:
+
+import sys
+import itertools
+import string
+from BitVector import *
+
+if len(sys.argv) is not 2:
+    sys.exit('''Needs one command line argument for the encrypted input''')
+
+def decrypt(encrypted_bv, key):
+    PassPhrase = "Hopes and dreams of a million years"
+    BLOCKSIZE = 16
+    key = key.strip()
+    numbytes = BLOCKSIZE // 8
+    # Reduce the passphrase to a bit array of size BLOCKSIZE:
+    bv_iv = BitVector(bitlist = [0]*BLOCKSIZE)
+    for i in range(0,len(PassPhrase) // numbytes):
+        textstr = PassPhrase[i*numbytes:(i+1)*numbytes]
+        bv_iv ^= BitVector( textstring = textstr )
+    # Reduce the key to a bit array of size BLOCKSIZE:
+    key_bv = BitVector(bitlist = [0]*BLOCKSIZE)
+    for i in range(0,len(key) // numbytes):
+        keyblock = key[i*numbytes:(i+1)*numbytes]
+        key_bv ^= BitVector( textstring = keyblock )
+    # Create a bitvector for storing the decrypted plaintext bit array:
+    msg_decrypted_bv = BitVector( size = 0 )
+    # Carry out differential XORing of bit blocks and decryption:
+    previous_decrypted_block = bv_iv
+    for i in range(0, len(encrypted_bv) // BLOCKSIZE):
+        bv = encrypted_bv[i*BLOCKSIZE:(i+1)*BLOCKSIZE]
+        temp = bv.deep_copy()
+        bv ^=  previous_decrypted_block
+        previous_decrypted_block = temp
+        bv ^=  key_bv
+        msg_decrypted_bv += bv
+    # Extract plaintext from the decrypted bitvector:    
+    return msg_decrypted_bv.get_text_from_bitvector()
+
+def iterations(charset, maxlength):
+    return (''.join(candidate)
+        for candidate in itertools.chain.from_iterable(itertools.product(charset, repeat=i)
+        for i in range(1, maxlength + 1)))
+
+def is_power2(num):
+    return num != 0 and ((num & (num - 1)) == 0)
+    
+def main():
+    print "BEGIN"
+    # Create a bitvector from the ciphertext hex string:
+    FILEIN = open(sys.argv[1])
+    encrypted_bv = BitVector( hexstring = FILEIN.read() )
+    gen = itertools.combinations(string.printable, 16)
+    for i, attempt in enumerate(iterations(string.printable, 16)):
+        out =  decrypt(encrypted_bv, attempt)
+        if(is_power2(i)):
+            print i
+        if(out.find("funerals") != -1):
+            print "Decrypted text: ",out
+            print "Key: ",attempt
+            break
+    print "COMPLETE"
+ 
+main()
