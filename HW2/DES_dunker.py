e47d44ca56559b4fe2caf100c54a3dd56a662955
Alex Dunker <adunker@purdue.edu>
2016-01-28 00:14:34 -0500

    Updated with working copy

--- HW2/DES_dunker.py
+++ HW2/DES_dunker.py
@@ -10,13 +10,6 @@ import sys
 import string
 from BitVector import *
 
-if len(sys.argv) is not 4:
-    sys.exit('''Usage: python DES_dunker.py message.txt key.txt [mode]\n\n\tModes: E - encrypt D - decrypt''')
-elif (sys.argv[3] != 'E' != sys.argv[3] != 'D'):
-    sys.exit('''Only modes are E - encrypt and D - decrypt''')
-
-################################   Initial setup  ################################
-
 # Expansion permutation (See Section 3.3.1):
 expansion_permutation = [31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8, 7, 8, 
 9, 10, 11, 12, 11, 12, 13, 14, 15, 16, 15, 16, 17, 18, 19, 20, 19, 
@@ -36,83 +29,92 @@ key_permutation_2 = [13,16,10,23,0,4,2,27,14,5,20,9,22,18,11,3,25,
 7,15,6,26,19,12,1,40,51,30,36,46,54,29,39,50,44,32,47,43,48,38,55,
 33,52,45,41,49,35,28,31]
 
-# Each integer here is the how much left-circular shift is applied
-# to each half of the 56-bit key in each round (See Section 3.3.5):
-shifts_key_halvs = [1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1] 
-
+key_shifts = [1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1] 
 
 ###################################   S-boxes  ##################################
 
 # Now create your s-boxes as an array of arrays by reading the contents
 # of the file s-box-tables.txt:
+arr = []
+s_box = []
 with open('s-box-tables.txt') as f:
-    arrays = []
-    s_box = []
     for line in f:
-        if len(line) > 4:
-            arrays.append(line.split())
+        if len(line) > 5:
+            arr.append(line.split())
     for i in range(0,32, 4):
-        s_box.append([arrays[k] for k in range(i, i+4)]) # S_BOX
+        s_box.append([arr[k] for k in range(i, i+4)])
 f.close()
 
-#######################  Get encryptin key from user  ###########################
+################################# Generate round keys  ########################
 
-def get_encryption_key(): # key                                                              
-    ## ask user for input
-    key = None
-    if sys.version_info[0] == 3:
-        key = input("\nEnter key: ")
-    else:                                                                         
-        key = raw_input("\nEnter key: ")
-        key = key.strip()
-    ## make sure it satisfies any constraints on the key
-    while len(key) != 8 or not key.isalpha():
-        print("Key should be 8 characters.")
-        if sys.version_info[0] == 3:
-            key = input("\nEnter key: ")
-        else:                                                                         
-            key = raw_input("\nEnter key: ")
-            key = key.strip()
-
-    ## next, construct a BitVector from the key    
-    user_key_bv = BitVector(textstring = key)   
-    key_bv = user_key_bv.permute(key_permutation_1)        ## permute() is a BitVector function
-    return key_bv
-
-
-################################# Generatubg round keys  ########################
-def extract_round_key( nkey ): # round key                                                   
+def create_keys(kBV):
+    subkeys = []
+    sBV = kBV.permute(key_permutation_1)
     for i in range(16):
-         [left,right] = nkey.divide_into_two()   ## divide_into_two() is a BitVector function
-         ## 
-         ##  the rest of the code
-         ##
-    return round_key
+        [left, right] = sBV.divide_into_two()
+        left << key_shifts[i]
+        right << key_shifts[i]
+        tBV = left + right
+        tBV = tBV.permute(key_permutation_2)
+        subkeys.append(tBV)        
+    return subkeys
 
+########################### Encryption / Decryption ###########################
 
-########################## encryption and decryption #############################
+def des(encrypt_or_decrypt, input_file, output_file, key ):
+    ## Get the key
+    kBV = BitVector( filename = key )
+    kBV = kBV.read_bits_from_file(64)
+    ## Get the round keys
+    rKeys = create_keys(kBV)
+    ## Create the bitvector for readint in the file
+    bv = BitVector( filename = input_file )
+    ## Prepare output file
+    FILEOUT = open( output_file, 'wb' )
+    ## Use the more_to_read function to travers whole file
+    while(bv.more_to_read):
+        ## Read 64 bit block
+        bitvec = bv.read_bits_from_file(64)
+        ## Ensure that length is 64, if not pad
+        if((bitvec.length() % 64) != 0):
+            bitvec.pad_from_right(64-(bitvec.length() % 64))
+        ## Use bitvector fucntion to split it into two
+        [LE, RE] = bitvec.divide_into_two()
+        ## Perform the 16 rounds of feistel
+        for i in range(16):
+            ## Place right in temp as it'll be the next left
+            TEMP = RE
+            ## Perform the expansion permutation
+            RE = RE.permute(expansion_permutation)
+            ## check if we're encrypting or decrypting
+            if encrypt_or_decrypt == 0:
+                RE = RE ^ rKeys[i] ## XOR with the roundkey
+            elif encrypt_or_decrypt == 1:
+                RE = RE ^ rKeys[15-i] ## reverse direction for decrypting
+            ## Initialize a bit vector to use
+            sBV = BitVector(size=0)
+            ## Perform the s-box functions for all 8
+            for j in range(8):
+                r = 2*RE[6*j] + 1*RE[5+6*j]
+                c = 8*RE[1+6*j] + 4*RE[2+6*j] + 2*RE[3+6*j] + 1*RE[4+6*j]
+                sBV += BitVector(intVal = int(s_box[j][r][c]), size=4)
+            ## Perform the p-permutation
+            pBV = sBV.permute(p_box_permutation)
+            ## Calculate the new right based of the left, and our f(RE,k)
+            RE = LE ^ pBV
+            ## Make the right the new left
+            LE = TEMP
+        ## Reverse the two and write it to the file
+        bitvec = RE + LE
+        FILEOUT.write(bitvec.get_text_from_bitvector())
+        return(bitvec.get_text_from_bitvector())
+    FILEOUT.close()
 
-def des(encrypt_or_decrypt, input_file, output_file, key ): 
-    bv = BitVector( filename = input_file ) 
-    FILEOUT = open( output_file, 'wb' ) 
-    bitvec = bv.read_bits_from_file( 64 )   ## assumes that your file has an integral
-                                            ## multiple of 8 bytes. If not, you must pad it.
-    print(bitvec)
-    print(bitvec.length())
-    [LE, RE] = bitvec.divide_into_two()      
-    #for i in range(16):        
-        ## write code to carry out 16 rounds of processing
-
-#################################### main #######################################
-
-def main():
-    ## write code that prompts the user for the key
-    key = get_encryption_key()
-    ## and then invokes the functionality of your implementation
-    print("Input file: %s." % sys.argv[1])
-    print("Output file: %s." % sys.argv[2])
-    des(sys.argv[3], sys.argv[1], sys.argv[2], key)
+########################### MAIN ##############################################
 
 if __name__ == "__main__":
-    main()
-
+    print("Encrypt / Decrypt function starting")
+    des(0, "message.txt", "encrypted.txt", "key.txt")
+    des(1, "encrypted.txt", "decrypted.txt", "key.txt")
+    print("Complete.")
+    
