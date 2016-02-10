c88abd57c8720c5bb6125297d8f02a8294395293
Alex Dunker <adunker2@gmail.com>
2016-02-10 14:30:19 -0500

    Feb 10th Updates

--- HW4/ece404_hw04_dunker.py
+++ HW4/ece404_hw04_dunker.py
@@ -1,9 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ### Author: Alex Dunker
 ### ECN: adunker
-### HW:
-### Filename:	
-### Due Date:
+### HW: 4
+### Filename: ece404_hw04_dunker.py
+### Due Date: 02/05/2016
 
-import sys
+message = "plaintext.txt"
+encout = "encryptedtext.txt"
+decout = "decryptedtext.txt"
+
+KEY = "howtogettosesame"
+
+from BitVector import *
+
+def roundKeys(key):
+    tmp = BitVector(textstring = key)
+    RK = BitVector(textstring = key)
+    #for i in range(10):
+    #    temp = RoundKey(
+
+def AES_enc(key, message, fileout):
+    keys = roundKeys(key)
+    return True
+
+def AES_dec(key, message, fileout):
+    return True
+
+def main():
+    kBV = BitVector( textstring = KEY )
+    mBV = BitVector( filename = message )
+    ENCOUT = open(encout, 'wb')
+    DECOUT = open(decout, 'wb')
+
+    while(mBV.more_to_read):
+        bitvec = mBV.read_bits_from_file(128)
+
+
+if __name__ == "__main__":
+    main()
