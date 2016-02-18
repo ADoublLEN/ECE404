ea410d815d73baa538f537f854f37f8ee51deec6
Alex Dunker <adunker@purdue.edu>
2016-02-18 13:20:49 -0500

    Finished HW5

--- HW5/hw05.py
+++ HW5/hw05.py
@@ -9,6 +9,7 @@
 import BitVector
 
 class RC4:
+    ## Initiali
     S = [i for i in range(256)]
 
     def __init__(self, key):
@@ -18,15 +19,15 @@ class RC4:
             j = (j + self.S[i] + ord(key[i % len(key)])) % 256
             self.S[i], self.S[j] = self.S[j], self.S[i]
 
-    def encrypt(self, image):
+    def RC4(self,image):
         img = []
-        with open(image, 'r') as f:
-            data = f.readlines()
-            for i in range(3, len(data)):
-                for j in range(len(data[i])):
-                    img.append(ord(data[i][j]))
+        if type(image[0]) is str:
+            for char in image:
+                img.append(ord(char))
+        else:
+            img = image
 
-        encryptS = self.S[:]
+        tempS = self.S[:]
 
         i = 0
         j = 0
@@ -35,20 +36,58 @@ class RC4:
 
         while True:
             i = (i + 1) % 256
-            j = (j + encryptS[i]) % 256
-            encryptS[i], encryptS[j] = encryptS[j], encryptS[i]
-            k = (encryptS[i] + encryptS[j]) % 256
-            encrypt.append(encryptS[k] ^ img[byte])
+            j = (j + tempS[i]) % 256
+            tempS[i], tempS[j] = tempS[j], tempS[i]
+            k = (tempS[i] + tempS[j]) % 256
+            encrypt.append(tempS[k] ^ img[byte])
             byte += 1
             if byte == len(img):
                 break
 
-        with open("encrypted_"+image, 'wba') as out:
-            for x in data[0:3]:
-                out.write(x)
-            out.write(bytearray(encrypt))
-        out.close()
         return encrypt
 
-    def decrypt(self, image):
-        return self.encrypt(image)
+    def encrypt(self, f):
+        ## Handles the file objects and reads them and then passes them to the RC4 algorithm
+        ## Then outputs the file to an "encryptedImage.ppm"
+        data = f.read()
+        encrypted = self.RC4(data)
+        f = open("encryptedImage.ppm", 'w+b')
+        f.write(bytearray(encrypted))
+        return f
+
+    def decrypt(self, f):
+        ## Handles the file objects and reads them and then passes them to the RC4 algorithm
+        ## Then outputs the file to an "decryptedImage.ppm"
+        data = f.read()
+        decrypted = self.RC4(data)
+        f = open("decryptedImage.ppm", 'wba')
+        f.write(bytearray(decrypted))
+        return f
+
+def main():
+
+    ## Personal testing for HW 5. Outputs the images with their headers so you can view them
+    rc4Cipher = RC4('keystring')
+    image = "winterTown.ppm"
+    with open(image, 'r') as f:
+        data = f.readlines()
+        header = data[0:3]
+        img = data[3:]
+    originalImage = "".join(img)
+    encryptedImage = rc4Cipher.RC4(originalImage)
+
+    with open("encrypted_"+image, 'wba') as out:
+        for x in header[0:3]:
+            out.write(x)
+        out.write(bytearray(encryptedImage))
+
+    decryptedImage = rc4Cipher.RC4(encryptedImage)
+    with open("decrypted_"+image, 'wba') as out:
+        for x in header[0:3]:
+            out.write(x)
+        out.write(bytearray(decryptedImage))
+
+
+
+if __name__ == "__main__":
+    main()
